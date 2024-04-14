89a522076443a9838ddc50c7d7c1fa8e416df754
Homing So <homingso@foxmail.com>
2024-04-14 21:06:37 +0800

    feat: initial the project
    Signed-off-by: Homing So <homingso@foxmail.com>

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    instrumentationHook: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/v1/chat/completions',
+        destination: '/api/v1/chat/completions',
+      },
+    ]
+  },
+}
+
+module.exports = withBundleAnalyzer(nextConfig)
